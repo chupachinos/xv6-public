ca8ca279a986cf6b9f7fef07401af9331a9ff84d
Zhong <zp.chomp@gmail.com>
2019-04-03 17:45:24 -0300

    Initial travis release

--- getprocs.c
+++ getprocs.c
@@ -1,12 +1,10 @@
 // Buffer cache.
 
 #include "types.h"
-#include "defs.h"
 #include "stat.h"
 #include "user.h"
-#include "proc.h"
 
 int main(void){
     printf(1,"El numero de procesos corriendo en el sistema es de: %d\n", getprocs());
-    exit();
+    exit()
 }
