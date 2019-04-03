60b418b6d293f35f581b84fd933c7b8d84320cb5
Zhong <zp.chomp@gmail.com>
2019-04-03 17:17:42 -0300

    Initial travis release

--- getprocs.c
+++ getprocs.c
@@ -6,7 +6,7 @@
 #include "user.h"
 #include "proc.h"
 
-int main(){
-    printf("El numero de procesos corriendo en el sistema es de: %d\n", getprocs());
+int main(void){
+    printf(1,"El numero de procesos corriendo en el sistema es de: %d\n", getprocs());
     exit();
 }
