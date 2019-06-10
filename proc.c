a6e63023b04434985b28c8bac5dc2140201b3c94
Zhong <zp.chomp@gmail.com>
2019-06-10 14:03:58 -0400

    lottery

--- proc.c
+++ proc.c
@@ -311,10 +311,10 @@ wait(void)
     sleep(curproc, &ptable.lock);  //DOC: wait-sleep
   }
 }
-static long
-lcg_rand(int a)
+static unsigned long
+lcg_rand(unsigned long a)
 {
-    long b = 279470273, c = 4294967291;
+    long unsigned b = (-279470273 -1), c = (-4294967291 -1);
     return (a * b) % c;
 }
 int n_tickets(void){
