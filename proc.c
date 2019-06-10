faa12a57543ef685f7f4184883b2364eb335f1bd
Zhong <zp.chomp@gmail.com>
2019-06-10 11:54:11 -0400

    lottery

--- proc.c
+++ proc.c
@@ -6,7 +6,6 @@
 #include "x86.h"
 #include "proc.h"
 #include "spinlock.h"
-#include<stdlib.h>
 
 struct {
   struct spinlock lock;
@@ -89,6 +88,7 @@ allocproc(void)
 found:
   p->state = EMBRYO;
   p->pid = nextpid++;
+  p->tickets = 10;
 
   release(&ptable.lock);
 
@@ -320,6 +320,12 @@ wait(void)
 //  - swtch to start running that process
 //  - eventually that process transfers control
 //      via swtch back to the scheduler.
+static
+unsigned long
+lcg_rand(unsigned long a){
+    unsigned long b = 279470273, c = 4294967291;
+    return (a*b)%c;
+}
 int n_tickets(void){
     struct proc *p;
     int total=0;
@@ -334,29 +340,29 @@ return total;
 void
 scheduler(void)
 {
+  int runval = 0;
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
   int number_tickets = 0;
   int counter = 0;
+  int winner = 0;
   for(;;){
-    // Enable interrupts on this processor.
+    runval++;
+      // Enable interrupts on this processor.
     sti();
     acquire(&ptable.lock);
     number_tickets = n_tickets(); // busca cuantos tickets hay en total
-    int winner = rand()%(number_tickets);
-    while (p->state != NULL){
-        counter+=p->tickets;
-        if (counter > winner){break;}
-        p->state = p->state -> next;
-        
-    }
-    
     // Loop over process table looking for process to run.
+    winner = lcg_rand(runval);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
+      if(counter + p->tickets)<(winner)
+          counter = counter + p->tickets;
+        continue;
         }
+      
 
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
